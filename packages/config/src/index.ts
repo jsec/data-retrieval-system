e9bcbdd67f67c30ef5822283013ca2d53c7263ed
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-08-02 17:24:08 -0400

    feat(logger): add basic logging package

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,14 +1,15 @@
 import { cleanEnv, str } from 'envalid';
 
 const env = cleanEnv(process.env, {
+    DATABASE_URL: str(),
     NODE_ENV: str({
         choices: ['development', 'test', 'production'],
         default: 'development',
     }),
-    STUFF: str({ default: 'stuff' }),
 });
 
 export const config = {
+    databaseUrl: env.DATABASE_URL,
     env: {
         dev: env.isDev,
         prod: env.isProd,
