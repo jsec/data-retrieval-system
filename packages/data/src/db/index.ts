9ab96c571f9d247affbef61b5fa47386ee0019cb
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-08-25 20:33:05 -0400

    feat(db): drizzle out, Kysely in

--- packages/data/src/db/index.ts
+++
@@ -1,4 +0,0 @@
-import { config } from '@hub/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-export const db = drizzle(config.databaseUrl);
--- packages/data/src/repositories/season.repository.ts
+++ packages/data/src/repositories/season.repository.ts
@@ -1,31 +1,30 @@
-import { eq, sum } from 'drizzle-orm';
+import { db } from '../db.js';
 
-import { db } from '../db/index.js';
-import { constructorResults, constructors, drivers, races, results } from '../db/schema.js';
-
-export const getDriverStandingsByYear = async (year: number) => {
-    return db.select({
-        driverId: drivers.id,
-        firstName: drivers.firstName,
-        lastName: drivers.lastName,
-        points: sum(results.points),
-    })
-        .from(results)
-        .innerJoin(drivers, eq(results.driverId, drivers.id))
-        .innerJoin(races, eq(results.raceId, races.id))
-        .where(eq(races.year, year))
-        .groupBy(drivers.id);
+export const getDriverPointsByYear = async (year: number) => {
+    return db.selectFrom('results')
+        .innerJoin('drivers', 'drivers.id', 'results.driverId')
+        .innerJoin('races', 'results.raceId', 'races.id')
+        .where('races.year', '=', year)
+        .groupBy('drivers.id')
+        .select([
+            'drivers.id as driverId',
+            'drivers.firstName',
+            'drivers.lastName',
+            eb => eb.fn.sum('results.points').as('points'),
+        ])
+        .execute();
 };
 
-export const getConstructorStandingsByYear = async (year: number) => {
-    return db.select({
-        constructorId: constructors.id,
-        constructorName: constructors.name,
-        points: sum(results.points),
-    })
-        .from(constructorResults)
-        .innerJoin(races, eq(constructorResults.raceId, races.id))
-        .innerJoin(constructors, eq(constructors.id, constructorResults.constructorId))
-        .where(eq(races.year, year))
-        .groupBy(constructors.id);
+export const getConstructorPointsByYer = async (year: number) => {
+    return db.selectFrom('constructorResults as cr')
+        .innerJoin('constructors as c', 'c.id', 'cr.constructorId')
+        .innerJoin('races as r', 'r.id', 'cr.raceId')
+        .where('r.year', '=', year)
+        .groupBy('c.id')
+        .select([
+            'c.id as constructorId',
+            'c.name as constructorName',
+            eb => eb.fn.sum('cr.points').as('points'),
+        ])
+        .execute();
 };
