b191dfaf54e4b1110277c14008a0bd6de23986cb
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-09-23 20:46:49 -0400

    style(web): update styling for shadcn blocks

--- apps/web/src/components/ui/skeleton.tsx
+++ apps/web/src/components/ui/skeleton.tsx
@@ -1,13 +1,13 @@
 import { cn } from "@/lib/utils"
+import type { ComponentProps } from "react"
 
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
-      {...props}
-    />
-  )
+export const Skeleton = ({ className, ...props }: ComponentProps<"div">) => {
+    return (
+        <div
+            data-slot="skeleton"
+            className={cn("bg-accent animate-pulse rounded-md", className)}
+            {...props}
+        />
+    )
 }
 
-export { Skeleton }
