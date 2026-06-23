78bd02909316c8d686ca102584966490dccf675f
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2026-06-23 18:34:15 -0400

    feat(web): add custom theme to Mantine provider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,4 +1,6 @@
-import { MantineProvider } from '@mantine/core';
+import type { MantineColorsTuple } from '@mantine/core';
+
+import { createTheme, MantineProvider } from '@mantine/core';
 import { TanStackDevtools } from '@tanstack/react-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
@@ -10,6 +12,24 @@ import TanStackQueryDevtools from './integrations/tanstack-query/devtools';
 import { getRouter } from './router';
 import './styles.css';
 
+const palette: MantineColorsTuple = [
+    '#ffeae6',
+    '#ffd4ce',
+    '#ffa79b',
+    '#ff7764',
+    '#fe4e36',
+    '#fe3519',
+    '#ff1e00',
+    '#e41800',
+    '#cb1000',
+    '#b20100',
+];
+
+const theme = createTheme({
+    colors: { palette },
+    primaryColor: 'palette',
+});
+
 const rootElement = document.querySelector('#app');
 
 if (!rootElement) {
@@ -20,7 +40,7 @@ const router = getRouter();
 
 createRoot(rootElement).render(
     <StrictMode>
-        <MantineProvider defaultColorScheme="light">
+        <MantineProvider defaultColorScheme="light" theme={theme}>
             <QueryClientProvider client={router.options.context.queryClient}>
                 <RouterProvider router={router} />
                 <TanStackDevtools
