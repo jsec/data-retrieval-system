0b89cf089e5e9315a96e7d046e133b218da1f601
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2026-06-25 11:06:07 -0400

    feat(db): add indexes to effone schema

---
+++ db/dbt/macros/create_index.sql
@@ -0,0 +1,10 @@
+{% macro create_index(index_name, columns, unique=false, using='btree') %}
+    create {% if unique %}unique {% endif %}index if not exists {{ adapter.quote(index_name) }}
+    on {{ this }}
+    {% if using %}using {{ using }}{% endif %}
+    (
+        {%- for column in columns -%}
+            {{ column }}{{ ", " if not loop.last }}
+        {%- endfor -%}
+    )
+{% endmacro %}
